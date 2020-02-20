17021b303736994b5920b88880cad98178f803f2
Darren Vasquez <dvasque4@gmail.com>
2020-02-20 01:09:47 -0700

    Adjusted Formatting, Continued Addition of Information

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { Container, Grid, Typography, Paper, Card, CardContent, CardActions, Button} from '@material-ui/core';
+import { Container, Grid, Typography, Paper, Card, CardContent, CardActions, Button, CardHeader, Avatar } from '@material-ui/core';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		flxGrow: 1,
+		flexGrow: 1,
 	},
 	intro: {
 		flexGrow: 1,
@@ -32,6 +32,14 @@ const useStyles = makeStyles(theme => ({
 	pos: {
 		marginBottom: 12,
 	},
+	header: {
+		paddingBottom: theme.spacing(3),
+		paddingLeft: 0,
+		paddingTop: 0,
+	},
+	cardAction: {
+		paddingTop: 0,
+	}
 }));
 
 export default function App() {
@@ -43,34 +51,25 @@ export default function App() {
 			<Typography variant='h2' align='center'>Darren Vasquez</Typography>
 			<Container maxwidth='md' className={classes.intro}>
 				<Typography variant='h5' className={classes.paper}>
-					<center>Hello! My name is Darren Vasquez.</center>
-					<br/>
-					I'm a senior at Arizona State University studying Computer Science. 
-					I currently work part-time with Intel as a Software Developer in Chandler, Arizona and will begin working with Google in the fall as a Software Engineer in Seattle, Washington.
-					<br/><br/>
-					<center>Check out my website to learn more about the work I'm involved in!</center>
+					<center>intro</center>
 				</Typography>
 			</Container>
 			<Container maxWidth='md' className={classes.root}>
-				EDUCATION
+				<Typography variant="h5" className={classes.paper}>Education</Typography>
 				<Grid container spacing={2}>
 					<Grid item xs={4}>
 						<Card className={classes.card}>
 							<CardContent>
-								<Typography className={classes.title} color="textSecondary" gutterBottom>
-									High School
-								</Typography>
-								<Typography variant="h5" component="h2">
-									Verrado HS
-								</Typography>
-								<Typography className={classes.pos} color="textSecondary">
-									Aug 2012 - May 2016
-								</Typography>
+								<CardHeader className={classes.header}
+									avatar={<Avatar aria-label="avatar" className={classes.avatar}>1</Avatar>}
+									title="Verrado High School"
+									subheader="Aug 2012 - May 2016"
+								/>
 								<Typography variant="body2" component="p">
 									Learned Graphic Design and Engineering principles
 								</Typography>
 							</CardContent>
-							<CardActions>
+							<CardActions className={classes.cardAction}>
 								<Button size="small" onClick={() => window.open("https://www.aguafria.org/vhs")}>Visit School Page</Button>
 							</CardActions>
 						</Card>
@@ -78,44 +77,97 @@ export default function App() {
 					<Grid item xs={4}>
 						<Card className={classes.card}>
 							<CardContent>
-								<Typography className={classes.title} color="textSecondary" gutterBottom>
-									Higher Education
-								</Typography>
-								<Typography variant="h5" component="h2">
-									Arizona State University
-								</Typography>
-								<Typography className={classes.pos} color="textSecondary">
-									Aug 2016 - Present
-								</Typography>
+								<CardHeader className={classes.header}
+									avatar={<Avatar aria-label="avatar" className={classes.avatar}>2</Avatar>}
+									title="Arizona State University"
+									subheader="Aug 2016 - May 2020"
+								/>
 								<Typography variant="body2" component="p">
 									Computer Science, B.S.
-									<br/>
+								</Typography>
+								<Typography variant="body2" component="p">
 									3.25 GPA
 								</Typography>
 							</CardContent>
-							<CardActions>
-								<Button size="small" onClick={() => window.open("https://asu.edu")}>Visit University Page</Button>
+							<CardActions className={classes.cardAction}>
+								<Button size="small" onClick={() => window.open("https://asu.edu")}>Visit School Page</Button>
 							</CardActions>
 						</Card>
 					</Grid>
 					<Grid item xs={4}>
 						<Card className={classes.card}>
 							<CardContent>
-								<Typography className={classes.title} color="textSecondary" gutterBottom>
-									Extracurricular
-								</Typography>
-								<Typography variant="h5" component="h2">
-									Luminosity Academy
-								</Typography>
-								<Typography className={classes.pos} color="textSecondary">
-									Aug 2019 - Dec 2019
-								</Typography>
+								<CardHeader className={classes.header}
+									avatar={<Avatar aria-label="avatar" className={classes.avatar}>3</Avatar>}
+									title="Luminosity Academy"
+									subheader="Aug 2019 - Dec 2019"
+								/>
 								<Typography variant="body2" component="p">
 									Learned fundamentals of React and how to work with a team on a project
 								</Typography>
 							</CardContent>
-							<CardActions>
-								<Button size="small" onClick={() => window.open("https://theluminositylab.com/academy/")}>Visit Academy Page</Button>
+							<CardActions className={classes.cardAction}>
+								<Button size="small" onClick={() => window.open("https://theluminositylab.com/academy/")}>Visit School Page</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+				</Grid>
+			</Container>
+			<br/>
+			<Container maxWidth='md' className={classes.root}>
+				<Typography variant="h5" className={classes.paper}>Experience</Typography>
+				<Grid container spacing={2}>
+					<Grid item xs={4}>
+						<Card className={classes.card}>
+							<CardContent>
+								<CardHeader className={classes.header}
+									avatar={<Avatar aria-label="avatar" className={classes.avatar}>1</Avatar>}
+									title="Intel Corporation"
+									subheader="Oct 2019 - ????"
+								/>
+								<Typography variant="body2" component="p">
+									Learned ?
+								</Typography>
+							</CardContent>
+							<CardActions className={classes.cardAction}>
+								<Button size="small" onClick={() => window.open("https://intel.com/")}>Visit Company Page</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+					<Grid item xs={4}>
+						<Card className={classes.card}>
+							<CardContent>
+								<CardHeader className={classes.header}
+									avatar={<Avatar aria-label="avatar" className={classes.avatar}>2</Avatar>}
+									title="SoDA @ ASU"
+									subheader="Oct 2018 - May 2020"
+								/>
+								<Typography variant="body2" component="p">
+									Computer Science, B.S.
+								</Typography>
+								<Typography variant="body2" component="p">
+									3.25 GPA
+								</Typography>
+							</CardContent>
+							<CardActions className={classes.cardAction}>
+								<Button size="small" onClick={() => window.open("https://thesoda.io")}>Visit Club Page</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+					<Grid item xs={4}>
+						<Card className={classes.card}>
+							<CardContent>
+								<CardHeader className={classes.header}
+									avatar={<Avatar aria-label="avatar" className={classes.avatar}>3</Avatar>}
+									title="Luminosity Academy"
+									subheader="Aug 2019 - Dec 2019"
+								/>
+								<Typography variant="body2" component="p">
+									Learned fundamentals of React and how to work with a team on a project
+								</Typography>
+							</CardContent>
+							<CardActions className={classes.cardAction}>
+								<Button size="small" onClick={() => window.open("https://theluminositylab.com/academy/")}>Visit School Page</Button>
 							</CardActions>
 						</Card>
 					</Grid>
